9e153505acd5a7970da66b44eb23969eaca362b8
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:43:00 +0530

    Create ForLoopDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/ForLoopDemo.java
@@ -0,0 +1,13 @@
+package org.tns.capgemini.c2tc.java;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
