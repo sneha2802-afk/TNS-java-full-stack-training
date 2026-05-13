06d4ada26a683e89c484a3a0f521110538ed734a
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:45:07 +0530

    Create LogicalOperatoDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/LogicalOperatoDemo.java
@@ -0,0 +1,25 @@
+package org.tns.capgemini.c2tc.java;
+
+public class LogicalOperatoDemo {
+
+	public static void main(String[] args) {
+		int a=10, b=5 ,c=20;
+		
+		//logical AND
+		System.out.println("(a>b) && (b<c):" + ((a>b) && (b<c)));
+		//1 * 1 =1
+				
+		//logical or
+		System.out.println("(a>b) || (b<c):"  + ((a>b) || (b<c)));
+		//1 + 1 =1
+		
+		
+		//logical NOT
+		System.out.println("!(a>b): "+ !(a>b));
+		
+		// true ~false
+		
+
+	}
+
+}
