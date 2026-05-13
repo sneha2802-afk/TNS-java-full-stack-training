80aaac5a6cf950b1feb6a8274e26c5b1ecba681d
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:22:08 +0530

    Create Finalvar.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /finaldemo /Finalvar.java
@@ -0,0 +1,12 @@
+package org.tns.capgemini.c2tc.finaldemo;
+
+public class Finalvar {
+
+	public static void main(String[] args) {
+		final int num=8;
+		num=9;
+		System.out.println(num);
+		
+	}
+
+}
