a8b1720326e7435101d88af2a84876f1ac3c456d
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:42:08 +0530

    Create DoWhileDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/DoWhileDemo.java
@@ -0,0 +1,15 @@
+package org.tns.capgemini.c2tc.java;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		
+			int i=1;
+			do {
+				//System.out.println("hello");
+				System.out.println(i);
+				i++;
+			}while(i<=10);
+	}
+
+}
