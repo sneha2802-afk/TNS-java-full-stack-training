1b62826a55e29ce7494db5e343c5886eea7b2700
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 14:04:32 +0530

    Create ScannerClassDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /scannerclass_bufferreader/ScannerClassDemo.java
@@ -0,0 +1,22 @@
+package org.tns.capgemini.c2tc.scannerclass_bufferreader;
+import java.util.Scanner;
+//Import the Scanner class
+//Create a scanner object
+//prompt the user for input
+//Read input using scanner method
+//Validate
+//Close the scanner
+public class ScannerClassDemo {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter your name");
+			 String name=scan.nextLine();
+			 System.out.println(name);
+		System.out.println("enter your age");
+			 int	age=scan.nextInt();
+			System.out.println(age);
+			scan.close();
+	}
+
+}
