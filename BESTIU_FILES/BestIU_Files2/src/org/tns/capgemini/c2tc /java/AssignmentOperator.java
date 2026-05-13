68493b7339704d997a06a1cc47aa397ee15eaf52
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:38:41 +0530

    Create AssignmentOperator.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/AssignmentOperator.java
@@ -0,0 +1,26 @@
+package org.tns.capgemini.c2tc.java;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		//add and assign
+		a+=5; //a=a+5
+		System.out.println("a after +=" +a);
+		
+		a-=3; // a=a-3
+		System.out.println("a after -="  +a);
+		
+		a*=2;  //a=a*2
+		System.out.println("a after *="  +a);
+		
+		a/=4; //a=a/4
+		System.out.println("a after /=" +a);
+		
+		a%=3; //a=a%3
+		System.out.println("a after %=" +a);
+
+	}
+
+}
