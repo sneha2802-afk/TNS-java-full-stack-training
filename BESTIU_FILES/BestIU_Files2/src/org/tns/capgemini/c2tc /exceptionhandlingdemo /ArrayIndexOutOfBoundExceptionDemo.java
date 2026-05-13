be7d01bc2937de1950b5dad68e6964937392d3b7
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:16:56 +0530

    Create ArrayIndexOutOfBoundExceptionDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /exceptionhandlingdemo /ArrayIndexOutOfBoundExceptionDemo.java
@@ -0,0 +1,20 @@
+package org.tns.capgemini.c2tc.exceptionhandlingdemo;
+
+public class ArrayIndexOutOfBoundExceptionDemo {
+
+	public static void main(String[] args) {
+		
+		 int arr[]=new int[4] ; //pos start =0 end =3   =5 pos
+		 
+		 try {
+			 int i =  arr[5];
+			 System.out.println(i);
+		 }catch (ArrayIndexOutOfBoundsException e)
+		 {
+			 System.out.println(e);
+		 }
+	     
+
+	}
+
+}
