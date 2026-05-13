c0294d51cc5f8f8171f928f96f5acc2162423b3c
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:29:26 +0530

    Create InterfaceDemo3.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /interfacedemo /InterfaceDemo3.java
@@ -0,0 +1,10 @@
+package org.tns.capgemini.c2tc.interfacedemo;
+import java.lang.FunctionalInterface;
+
+@FunctionalInterface
+public interface InterfaceDemo3 {
+	
+	public abstract void sayHello(String Name); //functional method
+
+
+}
