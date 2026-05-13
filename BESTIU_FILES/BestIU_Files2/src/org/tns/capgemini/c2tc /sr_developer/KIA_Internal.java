926de1e28d42140ae06eb78f1e48682e4b0ead0d
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 14:05:46 +0530

    Create KIA_Internal.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /sr_developer/KIA_Internal.java
@@ -0,0 +1,13 @@
+package org.tns.capgemini.c2tc.sr_developer;
+import org.tns.capgemini.c2tc.Jr_developer.*;
+public class KIA_Internal extends KIA_AirBags {
+
+	public static void main(String[] args) {
+		
+		KIA_Internal ob=new KIA_Internal();
+		ob.display_Air();
+		
+		
+	}
+
+}
