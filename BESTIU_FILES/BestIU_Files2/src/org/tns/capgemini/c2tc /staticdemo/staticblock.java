e540d597504f8d01c2b857d53cc0276531d3c4b3
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 14:07:17 +0530

    Create staticblock.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /staticdemo/staticblock.java
@@ -0,0 +1,28 @@
+package org.tns.capgemini.c2tc.staticdemo;
+
+
+ class StaticBlockDemo
+{
+	public void display()
+	{
+		System.out.println("instance block-1");
+	}
+	static
+	{
+		System.out.println("static block-1");
+	}
+	static
+	{
+		System.out.println("static block-2");
+	}
+}
+
+public class staticblock {
+
+	public static void main(String[] args) {
+		StaticBlockDemo obj=new StaticBlockDemo();
+		obj.display();
+
+	}
+
+}
