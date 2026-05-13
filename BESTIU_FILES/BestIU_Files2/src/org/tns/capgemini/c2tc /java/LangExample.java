e699bf1df88d0df5d938b7bf599424f085e8f99a
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:44:44 +0530

    Create LangExample.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/LangExample.java
@@ -0,0 +1,14 @@
+package org.tns.capgemini.c2tc.java;
+
+public class LangExample {
+
+	public static void main(String[] args) {
+		String greeting="hello world";
+		System.out.println("original"  +greeting);
+		System.out.println("upper case" + greeting.toUpperCase());
+		
+		int max=Math.max(10,20);
+		System.out.println("max" + max);
+	}
+
+}
