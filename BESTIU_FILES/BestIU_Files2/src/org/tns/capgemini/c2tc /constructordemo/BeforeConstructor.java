8a20f09762785279ae37c1c3872f483b8af3371b
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:14:34 +0530

    Create BeforeConstructor.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /constructordemo/BeforeConstructor.java
@@ -0,0 +1,17 @@
+package org.tns.capgemini.c2tc.constructordemo;
+
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+public class BeforeConstructor {
+
+	public static void main(String[] args) {
+		Myclass obj=new Myclass();
+		System.out.println(obj.color + " " + obj.cost + "  " + obj.Brand);
+
+	}
+
+}
