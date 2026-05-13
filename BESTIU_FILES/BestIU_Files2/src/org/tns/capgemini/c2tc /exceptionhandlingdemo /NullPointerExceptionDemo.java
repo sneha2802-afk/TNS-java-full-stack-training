e0a5e6164b91c840868bcb6f5377b107d8185bcc
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:18:36 +0530

    Create NullPointerExceptionDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /exceptionhandlingdemo /NullPointerExceptionDemo.java
@@ -0,0 +1,20 @@
+package org.tns.capgemini.c2tc.exceptionhandlingdemo;
+
+public class NullPointerExceptionDemo {
+
+	public static void main(String[] args) {
+		 String str=null;
+		 try {
+			 System.out.println(str.length()); 
+		 }catch (NullPointerException  e)
+		 {
+			 System.out.println(e);
+		 }
+		 finally
+		 {
+			 System.out.println("No matterwhat this block we get executed");
+		 }
+		 
+	}
+
+}
