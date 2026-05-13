183c48892fd4cade0a91aa4446b50cab4bd0d56b
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:45:31 +0530

    Create RelationalDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/RelationalDemo.java
@@ -0,0 +1,18 @@
+package org.tns.capgemini.c2tc.java;
+
+public class RelationalDemo {
+
+	public static void main(String[] args) {
+		
+		int a=10 ,b=5;
+		
+		System.out.println("a==b : " + (a==b)); //false
+		System.out.println("a!=b : " + (a!=b)); //true
+		System.out.println("a>b  :  "+ (a>b)); //true
+		System.out.println("a<b  :  "+ (a<b)); //false
+		System.out.println("a>=b :  "+ (a>=b)); //true
+		System.out.println("a<=b  : "+ (a<=b)); //false
+
+	}
+
+}
