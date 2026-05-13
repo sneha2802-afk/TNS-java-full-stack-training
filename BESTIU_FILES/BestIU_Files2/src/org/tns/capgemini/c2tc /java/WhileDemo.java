9477a8b32c6f25812e21efd1548b0fa91ef14a9e
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:47:51 +0530

    Create WhileDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/WhileDemo.java
@@ -0,0 +1,24 @@
+package org.tns.capgemini.c2tc.java;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+//		int i=1;
+//	
+////		while (i<10) {
+////		
+////		  System.out.println(i);
+////		  i++;
+////		}
+//		
+		int i=10;
+		
+		while (i>0) {
+		
+		  System.out.println(i);
+		  i--;
+		}
+
+	}
+
+}
