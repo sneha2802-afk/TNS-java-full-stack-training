52b41a97d674bbd6b08a5c9ab061d5a509acd4df
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:46:50 +0530

    Create TernaryOperator.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/TernaryOperator.java
@@ -0,0 +1,11 @@
+package org.tns.capgemini.c2tc.java;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        // Ternary operation
+        int max = (a > b) ? a : b;
+        System.out.println("Max value is: " + max);
+    }
+}
