200799deb4ed4f700d9577689a629a9d3b383306
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:42:31 +0530

    Create ForEachDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/ForEachDemo.java
@@ -0,0 +1,14 @@
+package org.tns.capgemini.c2tc.java;
+
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		int num[]= {2,3,4,5};
+		for (int print:num)
+		{
+			System.out.println(print);
+		}
+
+	}
+
+}
