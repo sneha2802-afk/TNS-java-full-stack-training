2774c9cb8a7aa46e85c60a2a600fd16ecbe63530
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:35:39 +0530

    Create ArithmeticOperator.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/ArithmeticOperator.java
@@ -0,0 +1,27 @@
+package org.tns.capgemini.c2tc.java;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		int a=10, b=5;
+		
+		int sum =a+b;//15
+		System.out.println("addition :" + sum);
+		
+		int diff =a-b; //5
+		System.out.println("sub : " + diff);
+		
+		int prod =a* b; //50
+		System.out.println("mul :" + prod);
+		
+		int quoitent =a/b; //2
+		System.out.println("division : " + quoitent);
+		
+		int remainder = a % b; //0
+		System.out.println("remainder : " + remainder );
+ 
+		
+		
+	}
+
+}
