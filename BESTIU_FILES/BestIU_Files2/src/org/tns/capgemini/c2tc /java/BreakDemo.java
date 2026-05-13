fdf4916de8269a4824c096720e56f4396e76a27b
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:39:25 +0530

    Create BreakDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/BreakDemo.java
@@ -0,0 +1,18 @@
+package org.tns.capgemini.c2tc.java;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<100;i++)
+		{
+			if(i>=51)
+			{
+				break;
+				
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
