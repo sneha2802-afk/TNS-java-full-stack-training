922c66f001dd09b452e7f39df7180f562f160e61
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:43:46 +0530

    Create IfDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/IfDemo.java
@@ -0,0 +1,17 @@
+package org.tns.capgemini.c2tc.java;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int age=11;
+		if (age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+	}
+
+}
