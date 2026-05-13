4ee9cccaaf13263e142e3f2a0eab01bc34d7cdf5
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:21:04 +0530

    Create FinalClassDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /finaldemo /FinalClassDemo.java
@@ -0,0 +1,23 @@
+package org.tns.capgemini.c2tc.finaldemo;
+
+final class ImplementByHema
+{
+	public void show()
+	{
+		System.out.println("Ai integration done by hema");
+	}
+}
+class Vishu extends ImplementByHema
+{
+	public void show()
+	{
+		System.out.println("Ai integration done by vishu");
+	}
+}
+public class FinalClassDemo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
