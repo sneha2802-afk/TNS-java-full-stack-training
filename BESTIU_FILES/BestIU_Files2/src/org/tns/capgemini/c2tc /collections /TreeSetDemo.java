d154f8547c93c5ef769978d647cbfb39461cc6ee
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:12:35 +0530

    Create TreeSetDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /collections /TreeSetDemo.java
@@ -0,0 +1,17 @@
+package org.tns.capgemini.c2tc.collections;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		TreeSet<String> set=new TreeSet<>();
+		set.add("Cherry");
+		set.add("Banana");
+		set.add("Apple");
+//		set.add(null); // it will not allow ,throws an exception
+		System.out.println("Treeset " +set);
+
+	}
+
+}
