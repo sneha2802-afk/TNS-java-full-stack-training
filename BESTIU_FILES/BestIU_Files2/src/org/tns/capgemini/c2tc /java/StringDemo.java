c7e9de939f7e6043d0fb6a99708a90a32302c5d2
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:46:06 +0530

    Create StringDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /java/StringDemo.java
@@ -0,0 +1,11 @@
+package org.tns.capgemini.c2tc.java;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String str="hello students welcome to the session";
+		System.out.println(str);
+
+	}
+
+}
