bf10beee443329d6a12a67db9864bbd71b8059f3
sneha2802-afk <snehmallikarjunrathod.28@gmail.com>
2026-05-13 13:19:37 +0530

    Create ThrowDemo.java

---
+++ BESTIU_FILES/BestIU_Files2/src/org/tns/capgemini/c2tc /exceptionhandlingdemo /ThrowDemo.java
@@ -0,0 +1,32 @@
+package org.tns.capgemini.c2tc.exceptionhandlingdemo;
+
+public class ThrowDemo {
+	
+	void validate(int age)
+	{
+		try {
+			
+	
+		if(age<18)
+		{
+			throw new ArithmeticException("you are not Eligible to C2TC");
+		}
+		else
+		{
+			System.out.println("You are Eligible");
+		}
+
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		ThrowDemo obj=new ThrowDemo();
+		obj.validate(5);
+		System.out.println("hello");
+
+	}
+
+}
